dff34c9bd8f603a79bbbfbf00730209e6d2ab479
jsyjst <y8588130@163.com>
2019-07-11 19:47:53 +0800

    优化Dialog布局

--- app/src/main/java/com/example/yuan_dialog/dialog/SpeedDialog.java
+++ app/src/main/java/com/example/yuan_dialog/dialog/SpeedDialog.java
@@ -53,8 +53,9 @@ public class SpeedDialog extends Dialog implements View.OnClickListener {
     private int mProgressColor;
 
     public static final int NORMAL_TYPE = 0; //正常中间确认框
-    public static final int BOTTOM_SELECT_TYPE = 1; //底部选择框
-    public static final int PROGRESS_TYPE = 2;//加载框
+    public static final int PROGRESS_TYPE = 1;//加载框
+    public static final int INPUT_TYPE = 2; //输入框
+    public static final int BOTTOM_SELECT_TYPE = 2; //底部选择框
 
 
     public interface OnYuanClickListener {
@@ -62,17 +63,16 @@ public class SpeedDialog extends Dialog implements View.OnClickListener {
     }
 
 
-
     public SpeedDialog(@NonNull Context context) {
-        this(context,NORMAL_TYPE);
+        this(context, NORMAL_TYPE);
     }
 
-    public SpeedDialog(Context context, int dialogType){
+    public SpeedDialog(Context context, int dialogType) {
         super(context, R.style.MyDialog);   //设置自己的风格
         mDialogType = dialogType;
-        if(mDialogType == PROGRESS_TYPE){
+        if (mDialogType == PROGRESS_TYPE) {
             setCanceledOnTouchOutside(false);
-        }else {
+        } else {
             setCancelable(true);  //点击屏幕和返回键，dialog消失
         }
 
@@ -82,17 +82,17 @@ public class SpeedDialog extends Dialog implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(mDialogType == BOTTOM_SELECT_TYPE){
+        if (mDialogType == BOTTOM_SELECT_TYPE) {
             setContentView(R.layout.dialog_bottom);
-        }else {
+        } else {
             View parentPanel = LayoutInflater.from(getContext()).inflate(R.layout.dialog_center, null);
             setContentView(parentPanel);
             mNormalPanel = parentPanel.findViewById(R.id.normalPanel);
             mProgressPanel = parentPanel.findViewById(R.id.progressPanel);
-            mTitleTv = parentPanel.findViewById(R.id.dialogTitle);        //标题
-            mMessageTv = parentPanel.findViewById(R.id.dialogText);    //内容
-            mCancelBtn = parentPanel.findViewById(R.id.dialogCancel);    //取消
-            mSureBtn = parentPanel.findViewById(R.id.dialogSure);    //确定
+            mTitleTv = parentPanel.findViewById(R.id.dialogTitleTv);        //标题
+            mMessageTv = parentPanel.findViewById(R.id.dialogMessageTv);    //内容
+            mCancelBtn = parentPanel.findViewById(R.id.dialogCancelBtn);    //取消
+            mSureBtn = parentPanel.findViewById(R.id.dialogSureBtn);    //确定
             mProgressBar = parentPanel.findViewById(R.id.progressBar);//加载框
             mProgressTv = parentPanel.findViewById(R.id.progressTv);//加载文字
 
@@ -112,22 +112,23 @@ public class SpeedDialog extends Dialog implements View.OnClickListener {
     }
 
 
-
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.dialogCancel:
+        switch (v.getId()) {
+            case R.id.dialogCancelBtn:
                 cancel();
-                if(mCancelClickListener != null) mCancelClickListener.onClick(this);
+                if (mCancelClickListener != null) mCancelClickListener.onClick(this);
                 break;
-            case R.id.dialogSure:
+            case R.id.dialogSureBtn:
                 cancel();
-                if(mSureClickListener !=null) mSureClickListener.onClick(this);
+                if (mSureClickListener != null) mSureClickListener.onClick(this);
                 break;
             default:
                 break;
         }
-    }/**
+    }
+
+    /**
      * 重新该方法，使dialog适应屏幕的宽高
      */
     @Override
@@ -135,102 +136,89 @@ public class SpeedDialog extends Dialog implements View.OnClickListener {
         super.show();
         Window dialogWindow = this.getWindow();
         assert dialogWindow != null;
-
-        if(mDialogType == BOTTOM_SELECT_TYPE) {
+        if (mDialogType == BOTTOM_SELECT_TYPE) {
             dialogWindow.setGravity(Gravity.BOTTOM); //设置位置
             dialogWindow.setWindowAnimations(R.style.bottom_menu_animation);//设置动画
-            dialogWindow.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-        }else {
-            WindowManager.LayoutParams lp = dialogWindow.getAttributes();
-            //获取屏幕宽度
-            DisplayMetrics dm = new DisplayMetrics();
-            WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-            assert wm != null;
-            wm.getDefaultDisplay().getMetrics(dm);
-            int screenWidth = dm.widthPixels;
-            //设置高宽
-            lp.width = (int) (screenWidth * 0.8); // 宽度
-            if(mDialogType == NORMAL_TYPE){
-                lp.height= (int) (screenWidth * 0.5);
-            }else {
-                lp.height= (int) (screenWidth * 0.4);
-            }
-            dialogWindow.setAttributes(lp);
+            dialogWindow.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         }
     }
 
     //设置标题
-    public SpeedDialog setTitle(String title){
+    public SpeedDialog setTitle(String title) {
         mTitleText = title;
-        if(mTitleTv != null&& mTitleText != null){
+        if (mTitleTv != null && mTitleText != null) {
             mTitleTv.setText(mTitleText);
         }
         return this;
     }
+
     //设置消息
-    public SpeedDialog setMessage(String text){
+    public SpeedDialog setMessage(String text) {
         mMessageText = text;
-        if(mMessageTv !=null && mMessageText != null){
+        if (mMessageTv != null && mMessageText != null) {
             mMessageTv.setText(mMessageText);
         }
         return this;
     }
 
     //设置取消按钮的文字
-    public SpeedDialog setCancelText(String cancel){
+    public SpeedDialog setCancelText(String cancel) {
         mCancelText = cancel;
-        if(mCancelText !=null&&mCancelBtn!= null){
+        if (mCancelText != null && mCancelBtn != null) {
             mCancelBtn.setText(mCancelText);
         }
         return this;
     }
+
     //设置确定按钮的文字
-    public SpeedDialog setSureText(String sure){
+    public SpeedDialog setSureText(String sure) {
         mSureText = sure;
-        if(mSureText !=null&&mSureBtn!= null){
+        if (mSureText != null && mSureBtn != null) {
             mSureBtn.setText(mSureText);
         }
         return this;
     }
+
     //设置确定按钮文字的颜色
-    public SpeedDialog setSureTextColor(int color){
+    public SpeedDialog setSureTextColor(int color) {
         mSureTextColor = color;
-        if(mSureBtn != null&&mSureTextColor != 0){
+        if (mSureBtn != null && mSureTextColor != 0) {
             mSureBtn.setTextColor(mSureTextColor);
         }
         return this;
     }
 
     //设置加载框的颜色
-    public SpeedDialog setProgressColor(int color){
+    public SpeedDialog setProgressColor(int color) {
         mProgressColor = color;
-        if(mProgressColor!= 0&&mProgressBar!=null){
+        if (mProgressColor != 0 && mProgressBar != null) {
             mProgressBar.setBarColor(color);
         }
         return this;
     }
-    public SpeedDialog setProgressText(String text){
+
+    public SpeedDialog setProgressText(String text) {
         mProgressText = text;
-        if(mProgressText!= null&&mProgressTv!=null){
+        if (mProgressText != null && mProgressTv != null) {
             mProgressTv.setText(mProgressText);
         }
         return this;
     }
 
     //设置确定按钮监听器
-    public SpeedDialog setSureClickListener(OnYuanClickListener sureClickListener){
+    public SpeedDialog setSureClickListener(OnYuanClickListener sureClickListener) {
         mSureClickListener = sureClickListener;
         return this;
     }
 
     //设置取消按钮监听器
-    public SpeedDialog setCancelClickListener(OnYuanClickListener cancelClickListener){
+    public SpeedDialog setCancelClickListener(OnYuanClickListener cancelClickListener) {
         mCancelClickListener = cancelClickListener;
         return this;
     }
 
-    private void showByDialogType(int dialogType){
-        switch (dialogType){
+    private void showByDialogType(int dialogType) {
+        switch (dialogType) {
             case NORMAL_TYPE:
                 mNormalPanel.setVisibility(View.VISIBLE);
                 mProgressPanel.setVisibility(View.GONE);
